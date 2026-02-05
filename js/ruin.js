f9d9b4f9dc84e90f0547ff1e32e8bf7eeeaa1cc3
Dan Grier <daniel.grier@gmail.com>
2026-02-05 16:21:38 +1000

    Allow raw string as name to encode

--- js/ruin.js
+++ js/ruin.js
@@ -1,7 +1,30 @@
-function replaceName() {
+const ENCODED_NAME_KEY = "n";
+const RAW_NAME_KEY = "r";
+
+function getNameFromSearchParams() {
 	const url = new URL(window.location.toLocaleString());
-	const urlParams = url.searchParams;
-	const n = urlParams.get("n");
+	const { searchParams } = url;
+	const encoded = searchParams.get(ENCODED_NAME_KEY);
+	const raw = searchParams.get(RAW_NAME_KEY);
+
+	if (raw) {
+		try {
+			const newEncoded = btoa(raw);
+			searchParams.set(ENCODED_NAME_KEY, newEncoded);
+			searchParams.delete(RAW_NAME_KEY);
+			window.location.search = searchParams;
+
+			return raw;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	return encoded || "";
+}
+
+function replaceName() {
+	const n = getNameFromSearchParams();
 
 	try {
 		const decodedName = atob(n);
